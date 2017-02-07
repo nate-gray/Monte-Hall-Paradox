101408b322a8213c0bd9c4b06dd6182d245f71a7
Nate Gray <ngray311@gmail.com>
2017-02-06 20:45:09 -0500

    Initial Commit

---
+++ MHP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHP
+//
+//  Created by Gray, Nathan on 2/6/17.
+//  Copyright © 2017 Gray, Nathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHP
+//
+//  Created by Gray, Nathan on 2/6/17.
+//  Copyright © 2017 Gray, Nathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
